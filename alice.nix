044c2ad3ead47b81292c1613cce245fc94637b21
alice <cashewnuts903+alice@gmail.com>
2025-05-09 21:22:33 +0000

    add gitui for alice.

--- alice.nix
+++ alice.nix
@@ -29,6 +29,7 @@
     # pkgs.hello
     pkgs.neovim
     pkgs.fzf
+    pkgs.gitui
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
