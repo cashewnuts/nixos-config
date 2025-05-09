82be7d2654516c5eb429409b120bfe998536e645
alice <cashewnuts903+alice@gmail.com>
2025-05-09 21:20:07 +0000

    add hyprland config

--- alice.nix
+++ alice.nix
@@ -4,6 +4,7 @@
 
   imports = [
     ./user/zsh.nix
+    ./user/hyprland.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
---
+++ user/hyprland.nix
@@ -0,0 +1,5 @@
+{ config, pkgs, ... }:
+
+{
+  home.file.".config/hypr/hyprland.conf".source = ./hyprland/hyprland.conf;
+}
