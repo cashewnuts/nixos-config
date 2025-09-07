20df4da6c6425ee456b1cbf7e5d0aba15b036e6d
alice <cashewnuts903+alice@gmail.com>
2025-09-07 19:45:59 +0000

    add waybar config.

--- user/hyprland.nix
+++ user/hyprland.nix
@@ -2,6 +2,8 @@
 
 {
   home.file.".config/hypr/hyprland.conf".source = ./hyprland/hyprland.conf;
+  home.file.".config/waybar/config.jsonc".source = ./waybar/config.jsonc;
+  home.file.".config/waybar/style.css".source = ./waybar/style.css;
   home.packages = [
     pkgs.walker
     pkgs.nautilus
