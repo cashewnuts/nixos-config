c22d0bd1c0cbd62db76897c9c379cc069bfab169
alice <cashewnuts903+alice@gmail.com>
2025-09-10 21:27:04 +0000

    update lsp setting for neovim.

--- user/neovim/default.nix
+++ user/neovim/default.nix
@@ -10,6 +10,9 @@
     ./treesitter.nix
     ./web-devicons.nix
     ./whichkey.nix
+    ./lsp/lsp.nix
+    ./lsp/confirm.nix
+    ./lsp/fidget.nix
   ];
 
   config = {
@@ -21,12 +24,12 @@
       # Line numbers
       number = true;
       relativenumber = true;
-      
+
       # Indentation
       tabstop = 4;
       shiftwidth = 4;
       expandtab = true;
-      
+
       # Search behavior
       ignorecase = true;
       smartcase = true;
@@ -40,49 +43,16 @@
     globalOpts = {
       # Mouse support
       mouse = "a";
-      
+
       # Clipboard integration
       clipboard = "unnamedplus";
-      
+
       # Update timing
       updatetime = 250;
     };
     localOpts = {
       # Concealment level for current buffer
       conceallevel = 0;
-    };
-    plugins = {
-      lsp = {
-        enable = true;
-        keymaps = {  
-          silent = true;  
-          diagnostic = {  
-            "<leader>k" = "goto_prev";  
-            "<leader>j" = "goto_next";  
-          };  
-          lspBuf = {  
-            "gd" = "definition";  
-            "gD" = "references";  
-            "gt" = "type_definition";  
-            "gi" = "implementation";  
-            "K" = "hover";  
-          };  
-          extra = [  
-            {  
-              key = "<leader>li";  
-              action = "<CMD>LspInfo<Enter>";  
-            }  
-            {  
-              key = "<leader>lr";  
-              action = "<CMD>LspRestart<Enter>";  
-            }  
-          ];  
-        };
-        servers = {
-          bashls.enable = true;
-          nil_ls.enable = true;
-        };
-      };
     };
   };
 }
