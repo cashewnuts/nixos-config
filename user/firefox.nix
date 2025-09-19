6262de6df2df5eae2e045a8c7ae5c856b623fde4
alice <cashewnuts903+alice@gmail.com>
2025-09-20 07:57:40 +0900

    update firefox ui in profile settings

--- user/firefox.nix
+++ user/firefox.nix
@@ -12,6 +12,8 @@
           "sidebar.main.tools" = "aichat,syncedtabs,history,bookmarks";
           "sidebar.revamp" = true;
           "sidebar.verticalTabs" = true;
+          "browser.uiCustomization.state" =
+            ''{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["ublock0_raymondhill_net-browser-action","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","_b9db16a4-6edc-47ec-a1f4-b86292ed211d_-browser-action"],"nav-bar":["sidebar-button","back-button","forward-button","firefox-view-button","urlbar-container","vertical-spacer","downloads-button","unified-extensions-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":[],"vertical-tabs":["tabbrowser-tabs"],"PersonalToolbar":["personal-bookmarks"]},"seen":["developer-button","_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action","ublock0_raymondhill_net-browser-action","_b9db16a4-6edc-47ec-a1f4-b86292ed211d_-browser-action","screenshot-button"],"dirtyAreaCache":["nav-bar","TabsToolbar","vertical-tabs","unified-extensions-area","toolbar-menubar","PersonalToolbar"],"currentVersion":23,"newElementCount":4}'';
         };
         search = {
           force = true;
