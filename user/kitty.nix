9f61328d691815941434128f88fec17e34329f17
alice <cashewnuts903+alice@gmail.com>
2025-09-24 15:31:17 +0900

    add kitty.conf for shell_integration conf.

--- user/kitty.nix
+++ user/kitty.nix
@@ -1,6 +1,9 @@
 { ... }:
 {
   home.file = {
+    ".config/kitty/kitty.conf" = {
+      source = ./kitty/kitty.conf;
+    };
     ".config/kitty/ssh.conf" = {
       source = ./kitty/ssh.conf;
     };
